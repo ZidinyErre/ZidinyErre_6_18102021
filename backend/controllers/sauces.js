a85e8d425d2764ebac7fe27666b3e57f962268c3
Zidiny Erre <mr.erre.zidiny@gmail.com>
2021-10-25 19:49:44 +0200

    fin de la premier partie des controllers

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -1,4 +1,5 @@
 const Sauce = require('../models/Sauce');
+const fs = require('fs');
 
 
 exports.getAllSauce = (req, res) => {
@@ -6,8 +7,46 @@ exports.getAllSauce = (req, res) => {
     .then(sauces => res.status(200).json(sauces))
     .catch(error => res.status(400).json({error}));
 };
+
 exports.getOneSauce = (req, res) => {
     Sauce.findOne({_id : req.params.id})
     .then(sauces => res.status(200).json(sauces))
     .catch(error => res.status(404).json({error}));
+};
+
+exports.createSauce = (req, res) => {
+    const sauceObject = JSON.parse(req.body.sauces);
+    delete sauceObject._id;
+    const sauce = new Sauce({
+        ...sauceObject,
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    });
+    sauce.save()
+    .then(() => res.status(201).json({message : 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({error}));
+};
+
+exports.modifySauce = (req, res) => {
+    const sauceObject = req.file ?
+    {
+        ...JSON.parse(req.body.sauces),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body};
+    Sauce.updateOne({_id: req.params.id},{ ...sauceObject, _id: req.params.id})
+    .then(() => res.status(200).json({message : 'Objet modifié !'}))
+    .catch(error => res.status(400).json({error}));
+
+};
+
+exports.deleteSauce = (req, res) => {
+    Sauce.findOne({ _id: req.params.id })
+    .then(sauce => {
+        const filename = sauce.imageUrl.split('/images/')[1];
+        fs.unlink(`images/${filename}`, () => {
+            Sauce.deleteOne({ _id: req.params.id })
+            .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+            .catch(error => res.status(400).json({ error }));
+        });
+      })
+    .catch(error => res.status(500).json({ error }));
 };
