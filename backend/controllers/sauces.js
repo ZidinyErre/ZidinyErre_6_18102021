ccc80609cfa480a7cbf0d8c72e9d340680f68693
Zidiny Erre <mr.erre.zidiny@gmail.com>
2021-10-29 18:43:41 +0200

    Probable résolution du premier post

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -2,31 +2,36 @@ const Sauce = require('../models/Sauce');
 const fs = require('fs');
 
 
-exports.getAllSauce = (req, res, next) => {
+exports.getAllSauce = (req, res) => {
     Sauce.find()
     .then(sauces => res.status(200).json(sauces))
     .catch(error => res.status(400).json({error}));
 };
 
-exports.getOneSauce = (req, res, next) => {
+exports.getOneSauce = (req, res) => {
     Sauce.findOne({_id : req.params.id})
     .then(sauces => res.status(200).json(sauces))
     .catch(error => res.status(404).json({error}));
 };
 
-exports.createSauce = (req, res, next) => {
+exports.createSauce = (req, res) => {
     const sauceObject = JSON.parse(req.body.sauces);
     delete sauceObject._id;
     const sauce = new Sauce({
         ...sauceObject,
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+        likes: 0,
+        dislikes: 0,
+        usersLiked: [],
+        usersDisliked: []
+
     });
     sauce.save()
     .then(() => res.status(201).json({message : 'Objet enregistré !'}))
     .catch(error => res.status(400).json({error}));
 };
 
-exports.modifySauce = (req, res, next) => {
+exports.modifySauce = (req, res) => {
     const sauceObject = req.file ?
     {
         ...JSON.parse(req.body.sauces),
@@ -38,7 +43,7 @@ exports.modifySauce = (req, res, next) => {
 
 };
 
-exports.deleteSauce = (req, res, next) => {
+exports.deleteSauce = (req, res) => {
     Sauce.findOne({ _id: req.params.id })
     .then(sauce => {
         const filename = sauce.imageUrl.split('/images/')[1];
@@ -49,4 +54,8 @@ exports.deleteSauce = (req, res, next) => {
         });
       })
     .catch(error => res.status(500).json({ error }));
+};
+
+exports.likesSauce = (req, res) => {
+
 };
