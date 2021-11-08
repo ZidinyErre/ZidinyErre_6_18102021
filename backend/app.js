6a232680a29dcc496a40714db9ec64ef6dc43015
Zidiny Erre <mr.erre.zidiny@gmail.com>
2021-11-08 15:48:23 +0100

    plus d'erreur cors mais erreur post

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-const cors = require('cors');
+// const cors = require('cors');
 const app = express();
 
 const userRoutes = require('./routes/user');
@@ -13,24 +13,37 @@ mongoose.connect('mongodb+srv://BruceWillis:billyboY44@thecluster.caibu.mongodb.
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-const corsOptions = {
-    origin: process.env.CLIENT_URL,
-    Credentials: true,
-    'allowedHeaders': ['sessionId', 'Content-Type'],
-    'exposedHeaders': ['sessionId'],
-    'methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-    'preflightContinue': false
-}
-app.use(cors(corsOptions));
+// const corsOptions = {
+//     origin: process.env.CLIENT_URL,
+//     Credentials: true,
+//     'allowedHeaders': ['sessionId', 'Content-Type'],
+//     'exposedHeaders': ['sessionId'],
+//     'methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+//     'preflightContinue': false
+// }
+// app.use(cors(corsOptions));
 
 // app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+//     res.writeHead(200,{
+//         'Access-Control-Allow-Origin': '*',
+//         'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization',
+//         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+//     })
 //     next();
-//   });
+// });
 
+app.use((req, res, next) => {
+    // w.WriteHeader(http.statusOK);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    
+    next();
+});
 
+app.options('/*', (_, res) => {
+    res.sendStatus(200);
+});
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(express.static('images'));
--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -5,12 +5,12 @@ const saucesCtrl = require('../controllers/sauces');
 const auth = require('../middlewares/auth');
 const multer = require('../middlewares/multer-config');
 
-router.get('/sauces', auth, saucesCtrl.getAllSauce);
-router.post('/sauces', auth, multer,saucesCtrl.createSauce);
-router.get('/sauces/:id', auth, saucesCtrl.getOneSauce);
-router.put('/sauces/:id', auth, multer,saucesCtrl.modifySauce);
-router.delete('/sauces/:id', auth, saucesCtrl.deleteSauce);
-router.post('/sauces/:id/like', auth,saucesCtrl.likesSauce);
+router.get('/api/sauces', auth, saucesCtrl.getAllSauce);
+router.post('/api/sauces', auth, multer,saucesCtrl.createSauce);
+router.get('/api/sauces/:id', auth, saucesCtrl.getOneSauce);
+router.put('/api/sauces/:id', auth, multer,saucesCtrl.modifySauce);
+router.delete('/api/sauces/:id', auth, saucesCtrl.deleteSauce);
+router.post('/api/sauces/:id/like', auth,saucesCtrl.likesSauce);
 
 
 module.exports = router;
