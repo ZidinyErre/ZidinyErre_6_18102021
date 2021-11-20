7ce44ae5ec999a7840c6e58d8a54665f3d8799d1
Zidiny Erre <mr.erre.zidiny@gmail.com>
2021-11-20 21:31:11 +0100

    Premier mise en place d'helmet

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const helmet = require('helmet');
 const mongoose = require('mongoose');
 const path = require('path');
-// const cors = require('cors');
 const app = express();
 
 const userRoutes = require('./routes/user');
@@ -13,27 +13,9 @@ mongoose.connect('mongodb+srv://BruceWillis:billyboY44@thecluster.caibu.mongodb.
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-// const corsOptions = {
-//     origin: process.env.CLIENT_URL,
-//     Credentials: true,
-//     'allowedHeaders': ['sessionId', 'Content-Type'],
-//     'exposedHeaders': ['sessionId'],
-//     'methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-//     'preflightContinue': false
-// }
-// app.use(cors(corsOptions));
 
-// app.use((req, res, next) => {
-//     res.writeHead(200,{
-//         'Access-Control-Allow-Origin': '*',
-//         'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization',
-//         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS'
-//     })
-//     next();
-// });
-
+app.use(helmet());
 app.use((req, res, next) => {
-    // w.WriteHeader(http.statusOK);
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
