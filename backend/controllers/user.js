dd97068f3654b665cf5b17278aa4c9fb7dbf1aa3
Zidiny Erre <mr.erre.zidiny@gmail.com>
2021-11-24 17:27:37 +0100

    dernier modif

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,21 +3,21 @@ const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-exports.signup = (req, res, next) => {
-bcrypt.hash(req.body.password, 10)
-.then(hash => {
-    const user = new User({
-        email: req.body.email,
-        password: hash
-    });
-    user.save()
-    .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-    .catch(error => res.status(400).json({error: 'Utilisateur non sauvegardé !'}));
-})
-.catch(error => res.status(500).json({error: 'Echec de l\'inscription !'}));
+exports.signup = (req, res) => {
+  bcrypt.hash(req.body.password, 10)
+  .then(hash => {
+      const user = new User({
+          email: req.body.email,
+          password: hash
+      });
+      user.save()
+      .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+      .catch(error => res.status(400).json({error: 'Utilisateur non sauvegardé ! L email est probablement déjà utilisé!'}));
+  })
+  .catch(error => res.status(500).json({error: 'Echec de l\'inscription !'}));
 };
 
-exports.login = (req, res, next) => {
+exports.login = (req, res) => {
   User.findOne({email: req.body.email})
 .then(user => {
     if (!user) {
